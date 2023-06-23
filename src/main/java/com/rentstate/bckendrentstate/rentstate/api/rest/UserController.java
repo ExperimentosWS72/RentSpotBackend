8a42a4fd4110fda1a7574069231b10dd0885ea8e
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-06-23 13:27:12 -0500

    CORS desactivado

--- src/main/java/com/rentstate/bckendrentstate/rentstate/api/rest/UserController.java
+++ src/main/java/com/rentstate/bckendrentstate/rentstate/api/rest/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
